74cc2ad0ab8213e31bda911a82d97fecacb16742
Roman Silivestrov <rsilivestr@gmail.com>
2024-11-24 22:27:07 +0300

    add header to the layout

--- editor/src/app/components/EditorView.tsx
+++ editor/src/app/components/EditorView.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Stage } from '@pixi/react';
-import { useElementSize, } from '../hooks/useResizeObserver';
+import { useElementSize } from '../hooks/useResizeObserver';
 import { EventStepView } from './EventStepView';
 
 export function EditorView() {
   const { ref, width, height } = useElementSize();
 
   return (
-    <div className="w-full bg-slate-900 grow-[1]" ref={ref}>
+    <div className="h-full" ref={ref}>
       <Stage
         width={width}
         height={height}
@@ -18,7 +18,7 @@ export function EditorView() {
           backgroundColor: '#888',
         }}
       >
-        <EventStepView id='abc' text='hello world!' position={[0,0]} />
+        <EventStepView id="abc" text="hello world!" position={[0, 0]} />
       </Stage>
     </div>
   );
--- editor/src/app/layout.tsx
+++ editor/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
 import './globals.css';
+import Link from 'next/link';
 
 const firaCode = localFont({
   src: './fonts/FiraCodeVF.woff2',
@@ -12,7 +13,7 @@ const firaCode = localFont({
 
 export const metadata: Metadata = {
   title: 'Arhm Editr',
-  icons: ['favicon.svg']
+  icons: ['favicon.svg'],
 };
 
 export default function RootLayout({
@@ -22,7 +23,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${firaCode.variable} antialiased`}>{children}</body>
+      <body
+        className={`flex flex-col min-h-screen ${firaCode.variable} antialiased`}
+      >
+        <header className="p-4 flex text-white bg-pink-800">
+          <Link href="/" className="text-lg font-bold">
+            Arhm Editr
+          </Link>
+        </header>
+        <main className="h-0 grow bg-slate-100 dark:bg-slate-900">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
