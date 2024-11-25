fc388b358946b5dd6246da7533aaf1b4590229f3
Roman Silivestrov <rsilivestr@gmail.com>
2024-11-25 23:58:51 +0300

    add login form request

--- editor/src/app/login/page.tsx
+++ editor/src/app/login/page.tsx
@@ -1,15 +1,54 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import ky from 'ky';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+
+import { API_BASE_URL } from '@/config';
+import { setToken, setUsername } from '@/utils/storage';
+
+type UserCredentials = {
+  username: string;
+  password: string;
+};
+
+type UserLoginResponse = {
+  token: string;
+  message: string;
+};
+
 export default function Login() {
+  const router = useRouter();
+
+  const { isError, mutate: login } = useMutation({
+    mutationFn: async (json: UserCredentials) => {
+      return await ky
+        .post(`${API_BASE_URL}/user_login`, { json })
+        .json<UserLoginResponse>();
+    },
+    onSuccess: ({ token }, { username }) => {
+      setToken(token);
+      setUsername(username);
+      router.push('/');
+    },
+  });
+
+  const { register, handleSubmit } = useForm<UserCredentials>();
+
   return (
     <div className="h-full py-64 flex justify-center">
-      <form className="flex flex-col gap-4">
+      <form
+        className="w-[300px] flex flex-col gap-4"
+        onSubmit={handleSubmit((cred) => login(cred))}
+      >
         <label className="flex flex-col gap-1">
           <span className="text-sm font-bold">Логин</span>
           <input
             className="px-2 py-1 dark:bg-slate-700"
             type="text"
-            name="username"
             autoComplete="username"
-            required
+            {...register('username', { required: true })}
           />
         </label>
         <label className="flex flex-col gap-1">
@@ -17,9 +56,8 @@ export default function Login() {
           <input
             className="px-2 py-1 dark:bg-slate-700"
             type="password"
-            name="password"
             autoComplete="current-password"
-            required
+            {...register('password', { required: true })}
           />
         </label>
         <button
@@ -28,6 +66,11 @@ export default function Login() {
         >
           Войти
         </button>
+        {isError && (
+          <p className="text-red-600 dark:text-red-400">
+            Неверное имя пользователя или пароль
+          </p>
+        )}
       </form>
     </div>
   );
