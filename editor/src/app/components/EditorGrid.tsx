a9417cea30064be7bc0a993ebccf7c6626b4caa4
Roman Silivestrov <rsilivestr@gmail.com>
2024-11-29 09:33:18 +0300

    add grid cell hover state; split grid gap into x and y

--- editor/src/app/components/EditorGrid.tsx
+++ editor/src/app/components/EditorGrid.tsx
@@ -1,16 +1,19 @@
-import { Container, Graphics } from '@pixi/react';
+import { Container } from '@pixi/react';
 import { useMemo } from 'react';
 
 import {
   EDITOR_CELL_HEIGHT,
   EDITOR_CELL_WIDTH,
-  EDITOR_GRID_GAP,
+  EDITOR_GRID_GAP_X,
+  EDITOR_GRID_GAP_Y,
   EDITOR_PADDING,
 } from '@/config';
 import { useEditorGrid } from '@/store/editor/grid';
 
-const colOffset = EDITOR_CELL_WIDTH + EDITOR_GRID_GAP;
-const rowOffset = EDITOR_CELL_HEIGHT + EDITOR_GRID_GAP;
+import { EditorGridCell } from './EditorGridCell';
+
+const colOffset = EDITOR_CELL_WIDTH + EDITOR_GRID_GAP_X;
+const rowOffset = EDITOR_CELL_HEIGHT + EDITOR_GRID_GAP_Y;
 
 export function EditorGrid() {
   const grid = useEditorGrid();
@@ -25,16 +28,10 @@ export function EditorGrid() {
         const colIndex = index % grid.columns;
         const rowIndex = Math.floor(index / grid.rows);
         return (
-          <Graphics
+          <EditorGridCell
             key={`${colIndex}/${rowIndex}`}
             x={colIndex * colOffset}
             y={rowIndex * rowOffset}
-            draw={(g) => {
-              g.clear();
-              g.beginFill('#334155');
-              g.drawRect(0, 0, EDITOR_CELL_WIDTH, EDITOR_CELL_HEIGHT);
-              g.endFill();
-            }}
           />
         );
       })}
