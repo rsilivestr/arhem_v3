390d4c0445ab0e54c6bc4a946342247af15d6a05
Олег Медовиков <oleg.medovikov@mail.ru>
2024-12-04 23:57:13 +0300

    add register

--- src/web/editor.rs
+++ src/web/editor.rs
@@ -13,3 +13,9 @@ pub fn editor() -> RawHtml<String> {
     let content = fs::read_to_string("static/editor/index.html").expect("Unable to read file");
     RawHtml(content)
 }
+
+#[get("/editor/register")]
+pub fn register() -> RawHtml<String> {
+    let content = fs::read_to_string("static/editor/register.html").expect("Unable to read file");
+    RawHtml(content)
+}
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -2,10 +2,10 @@ use rocket::routes;
 
 mod editor;
 
-
 pub fn web_routes() -> Vec<rocket::Route> {
     routes![
         editor::login,
         editor::editor,
+        editor::register,
     ]
 }
