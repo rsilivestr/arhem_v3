7571bd813eeb0d40f155ffdabb4148b4db8d4453
oleg-medovikov <oleg.medovikov@mail.ru>
2024-12-05 11:48:59 +0300

    отправил sql to files

--- src/base/create_tables.rs
+++ src/base/create_tables.rs
@@ -4,16 +4,16 @@ use std::path::Path;
 
 pub async fn create_tables(pool: &PgPool) -> Result<(), sqlx::Error> {
     let sql_files = vec![
-        "01_users_table.sql",
-        "02_users_log_text_table.sql",
-        "03_users_log_table.sql",
-        "04_locations_table.sql",
-        "05_events_table.sql",
-        "06_event_steps_table.sql",
-        "07_event_x_steps_table.sql",
-        "08_event_links_table.sql",
-        "09_event_options_table.sql",
-        "10_event_links_x_options_table.sql",
+        "users_table.sql",
+        "users_log_text_table.sql",
+        "users_log_table.sql",
+        "locations_table.sql",
+        "events_table.sql",
+        "event_steps_table.sql",
+        "event_x_steps_table.sql",
+        "event_links_table.sql",
+        "event_options_table.sql",
+        "event_links_x_options_table.sql",
     ];
 
     for file_name in sql_files {
