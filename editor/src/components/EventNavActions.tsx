8fd344e1975b6ad79320ef5028f0e567ea764c4a
Roman Silivestrov <rsilivestr@gmail.com>
2024-12-13 22:06:34 +0300

    rearrange tabs

--- editor/src/components/EventNavActions.tsx
+++ editor/src/components/EventNavActions.tsx
@@ -36,7 +36,16 @@ export function EventNavActions({ filterTerm, onFilterChange }: Props) {
     <Tabs.Root defaultValue="none" value={tab} onValueChange={setTab}>
       <Tabs.List className="relative flex w-full h-10 bg-slate-350 dark:bg-slate-800">
         <Tabs.Trigger
-          value="create"
+          value="event-search"
+          aria-label="Найти ивенты"
+          title="Найти ивенты"
+          className="data-[state=active]:bg-slate-300 hover:bg-slate-400 dark:data-[state=active]:bg-slate-900 dark:hover:bg-slate-700"
+          onPointerDown={handleTabPointerDown}
+        >
+          <MagnifyingGlassIcon className="h-10 w-10 p-2 " />
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="event-create"
           aria-label="Создать ивент"
           title="Создать ивент"
           className="data-[state=active]:bg-slate-300 enabled:hover:bg-slate-400 dark:data-[state=active]:bg-slate-900 enabled:dark:hover:bg-slate-700 disabled:cursor-not-allowed"
@@ -46,7 +55,7 @@ export function EventNavActions({ filterTerm, onFilterChange }: Props) {
         </Tabs.Trigger>
         <Tabs.Trigger
           disabled={!event}
-          value="edit"
+          value="event-edit"
           aria-label={
             event ? 'Редактировать ивент' : 'Не выбран ивент для редактирования'
           }
@@ -58,23 +67,14 @@ export function EventNavActions({ filterTerm, onFilterChange }: Props) {
         >
           <Pencil1Icon className="h-10 w-10 p-2 " />
         </Tabs.Trigger>
-        <Tabs.Trigger
-          value="search"
-          aria-label="Найти ивенты"
-          title="Найти ивенты"
-          className="data-[state=active]:bg-slate-300 hover:bg-slate-400 dark:data-[state=active]:bg-slate-900 dark:hover:bg-slate-700"
-          onPointerDown={handleTabPointerDown}
-        >
-          <MagnifyingGlassIcon className="h-10 w-10 p-2 " />
-        </Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content value="create" className="p-4">
+      <Tabs.Content value="event-create" className="p-4">
         <EventCreateForm />
       </Tabs.Content>
-      <Tabs.Content value="edit" className="p-4">
+      <Tabs.Content value="event-edit" className="p-4">
         <EventEditForm event={event!} />
       </Tabs.Content>
-      <Tabs.Content value="search" className="p-4">
+      <Tabs.Content value="event-search" className="p-4">
         <TextField
           label="Искать по названию и коду"
           value={filterTerm}
