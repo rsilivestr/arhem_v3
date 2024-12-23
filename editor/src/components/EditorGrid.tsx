04630b3332089b5fc69a0d6934f2b4ec05c42350
Roman Silivestrov <rsilivestr@gmail.com>
2024-12-23 21:28:52 +0300

    trim editor store

--- editor/src/components/EditorGrid.tsx
+++ editor/src/components/EditorGrid.tsx
@@ -18,32 +18,36 @@ const rowOffset = EDITOR_CELL_HEIGHT + EDITOR_GRID_GAP_Y;
 export function EditorGrid() {
   const grid = useEditorGrid();
   const cells: (GameStep | null)[] = useMemo(() => {
-    const _cells = Array(grid.cols * grid.rows).fill(null);
+    if (!grid.event) {
+      return [];
+    }
+    const _cells = Array(grid.event.max_cols * grid.event.max_rows).fill(null);
     grid.steps.forEach(({ step }) => {
       console.log(step);
-      const stepIndex = (step.row - 1) * grid.cols + (step.col - 1);
+      const stepIndex = (step.row - 1) * grid.event!.max_cols + (step.col - 1);
       console.log(stepIndex);
       _cells[stepIndex] = step;
     });
     return _cells;
-  }, [grid.cols, grid.rows, grid.steps]);
+  }, [grid.event, grid.steps]);
 
   return (
     <Container x={EDITOR_PADDING} y={EDITOR_PADDING}>
-      {cells.map((step, index) => {
-        const colIndex = index % grid.cols;
-        const rowIndex = Math.floor(index / grid.rows);
-        return (
-          <EditorGridCell
-            key={`${colIndex}/${rowIndex}`}
-            x={colIndex * colOffset}
-            y={rowIndex * rowOffset}
-            col={colIndex + 1}
-            row={rowIndex + 1}
-            step={step}
-          />
-        );
-      })}
+      {grid.event &&
+        cells.map((step, index) => {
+          const colIndex = index % grid.event!.max_cols;
+          const rowIndex = Math.floor(index / grid.event!.max_rows);
+          return (
+            <EditorGridCell
+              key={`${colIndex}/${rowIndex}`}
+              x={colIndex * colOffset}
+              y={rowIndex * rowOffset}
+              col={colIndex + 1}
+              row={rowIndex + 1}
+              step={step}
+            />
+          );
+        })}
     </Container>
   );
 }
--- editor/src/store/editor/grid.ts
+++ editor/src/store/editor/grid.ts
@@ -8,12 +8,6 @@ type Cell = {
 };
 
 type EditorGrid = {
-  cols: number;
-  rows: number;
-  setSize(columns: number, rows: number): void;
-  addCols(amount: number): void;
-  addRows(count: number): void;
-
   activeCell: Cell | null;
   setActiveCell(cell: Cell | null): void;
 
@@ -28,29 +22,6 @@ type EditorGrid = {
 };
 
 const useEditorGrid = create<EditorGrid>((set) => ({
-  cols: 5,
-  rows: 5,
-
-  setSize(cols, rows) {
-    if (isValidSize(cols) && isValidSize(rows)) {
-      set({ cols, rows });
-    }
-  },
-
-  addCols(amount) {
-    set((grid) => {
-      const cols = grid.cols + amount;
-      return isValidSize(cols) ? { ...grid, cols } : grid;
-    });
-  },
-
-  addRows(amount) {
-    set((grid) => {
-      const rows = grid.rows + amount;
-      return isValidSize(rows) ? { ...grid, rows } : grid;
-    });
-  },
-
   activeCell: null,
 
   setActiveCell(activeCell) {
@@ -72,9 +43,5 @@ const useEditorGrid = create<EditorGrid>((set) => ({
     set({ activeStep });
   },
 }));
-
-function isValidSize(n: number) {
-  return Number.isInteger(n) && n > 0 && n <= Number.MAX_SAFE_INTEGER;
-}
 
 export { useEditorGrid };
